8e02ce19225a7493755a7c93699211a226931d7a
Paxton Warin <paxton@Paxtons-MacBook-Pro.local>
2024-05-02 08:46:48 -0400

    a

--- static/index.js
+++ static/index.js
@@ -1,3 +1,9 @@
+function checkMobile() {
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    return false;
+}
+}
 window.tHs = new ThemeSystem();
 
 var darkTheme = new Theme(document.getElementById("default-theme"), "Moon"); // I do not have programming ineptitude, it's just that this works and I don't really care to change it from the older version of the theme system.
@@ -693,7 +699,7 @@ const ts = new TabSystem({
 
 const createNewTab = () => {
   ts.setActiveTab(ts.addTab(new Tab()));
-  ts.activeTab.getTabElement().querySelector("#invite").innerText = Agent;/*splashtext[Math.floor(Math.random() * splashtext.length)]*/
+  ts.activeTab.getTabElement().querySelector("#invite").innerText = checkMobile();/*splashtext[Math.floor(Math.random() * splashtext.length)]*/
 };
 
 createNewTab();
